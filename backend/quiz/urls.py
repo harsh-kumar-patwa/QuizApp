df6e153fb3fb36a9e948c81fa2df7dce6d17968c
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-12-30 00:17:31 +0530

    added urls for the views

---
+++ backend/quiz/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from .views import RegisterView, LoginView, QuizListCreateView, QuizDetailView, QuizAttemptView, QuizSubmitView
+
+urlpatterns  = [
+    path('register/', RegisterView.as_view(), name='register'),
+    path('login/', LoginView.as_view(), name='login'),
+    path('quizzes/', QuizListCreateView.as_view(), name='quiz-list-create'),
+    path('quizzes/<int:pk>/', QuizDetailView.as_view(), name='quiz-detail'),
+    path('quizzes/<int:quiz-id>/attempt/', QuizAttemptView.as_view(), name='quiz-attempt'),
+    path('attempts/<int:attempt-id>/submit/', QuizSubmitView.as_view(), name='quiz-submit'),
+]
