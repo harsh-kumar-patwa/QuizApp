c1193842ec1bf0ce774d27ae63082892e6757c79
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-12-30 00:11:03 +0530

    added quizDetail view class and get put delete methods

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -61,3 +61,47 @@ class QuizListCreateView(APIView):
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class QuizDetailView(APIView):
+    permission_classes = (permissions.IsAuthenticated,)
+
+    def get_object(self, pk):
+        try:
+            return Quiz.objects.get(pk=pk)
+        except Quiz.DoesNotExist:
+            return None
+    
+    def get(self, request, pk):
+
+        quiz = self.get_object(pk)
+        if quiz:
+            serializer = QuizSerializer(quiz)
+            return Response(serializer.data)
+        return Response({'error': 'Quiz not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+    def put(self, request, pk):
+        if request.user.user_types != 'admin':
+            return Response({'error': 'You are not authorized to update quiz'}, status=status.HTTP_403_FORBIDDEN)
+        
+        quiz = self.get_object(pk)
+        if not quiz:
+            return Response({'error': 'Quiz not found'}, status=status.HTTP_404_NOT_FOUND)
+        
+        serializer = QuizSerializer(quiz, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
+    def delete(self, request, pk):
+        if request.user.user_types != 'admin':
+            return Response({'error': 'You are not authorized to delete quiz'}, status=status.HTTP_403_FORBIDDEN)
+        
+        quiz = self.get_object(pk)
+        if quiz:
+            quiz.delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        
+        return Response({'error': 'Quiz not found'}, status=status.HTTP_404_NOT_FOUND)
+    
+
