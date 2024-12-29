6d40e1dec7b3cb797f0893cdc7467f8591b2f9e9
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-12-30 00:08:39 +0530

    added registerview class and methods

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework import status,permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from django.utils import timezone
+from .models import User, Quiz, Question, QuizAttempt
+from .serializers import UserSerializer, QuizSerializer, QuestionSerializer, QuizAttemptSerializer  
 
-# Create your views here.
+class RegisterView(APIView):
+    permission_classes = (permissions.AllowAny,)
+
+    def post(self, request):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            refresh = RefreshToken.for_user(user)
+            return Response({
+                'user': serializer.data,
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            },status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
