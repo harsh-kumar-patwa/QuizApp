6b6ed6d9b148870e0d73584278ec42a6f3de5ad0
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-12-30 00:33:46 +0530

    added admin interface

--- backend/quiz/admin.py
+++ backend/quiz/admin.py
@@ -1,3 +1,27 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User, Quiz, Question, QuizAttempt
 
-# Register your models here.
+class CustomUserAdmin(UserAdmin):
+    list_display = ('username', 'email', 'user_type','is_staff')
+    list_filter = ('user_type','is_staff','is_superuser')
+    fieldsets = UserAdmin.fieldsets + (
+        ('Additional Info',{'fields':('user_type',)}),
+    )
+
+class QuestionInline(admin.TabularInline):
+    model = Question
+    extra = 1
+
+class QuizAdmin(admin.ModelAdmin):
+    list_display = ('title', 'created_by', 'created_at', 'updated_at') 
+    search_fields = ('title', 'description')
+    inlines = [QuestionInline]
+
+class QuizAttemptAdmin(admin.ModelAdmin):
+    list_display = ('user', 'quiz', 'score', 'started_at', 'completed_at')
+    list_filter = ('quiz', 'user')
+
+admin.site.register(User, CustomUserAdmin)
+admin.site.register(Quiz, QuizAdmin)
+admin.site.register(QuizAttempt, QuizAttemptAdmin)
