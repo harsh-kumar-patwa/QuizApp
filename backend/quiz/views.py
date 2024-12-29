07db8903ada6d8b06dad5c26f97d650f7e4604f4
Harsh Kumar <harsh.23bcs10021@ms.sst.scaler.com>
2024-12-30 00:09:12 +0530

    added loginview class and post method

--- backend/quiz/views.py
+++ backend/quiz/views.py
@@ -23,3 +23,22 @@ class RegisterView(APIView):
             },status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
+class LoginView(APIView):
+    permission_classes  = (permissions.AllowAny,)
+
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            refresh = RefreshToken.for_user(user)
+            serializer = UserSerializer(user)
+            return Response({
+                'user': serializer.data,
+                'refresh': str(refresh),
+                'access': str(refresh.access_token),
+            })
+        return Response({'error': 'Invalid credentials'}, status=status.HTTP_400_BAD_REQUEST)
+    
